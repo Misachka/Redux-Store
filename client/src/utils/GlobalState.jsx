e375168f538abf3cd12de13bcf8cc2643eab1730
Misachka <136197530+Misachka@users.noreply.github.com>
2023-12-09 15:52:52 -0500

    added notes to clobalstate

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,23 +1,32 @@
-import { createContext, useContext, useReducer } from "react";
-import { reducer } from './reducers'
+//createContext will be used to instantiate a new Context object.
+//useContext is another React Hook that will allow us to use the state created from the createContext function.
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
 
+// This function creates a new context object 
 const StoreContext = createContext();
+
+// Every Context object comes with two components, a Provider and Consumer
 const { Provider } = StoreContext;
 
+//StoreProvider instantiate  initial global state with the useProductReducer() function
 const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useReducer(reducer, {
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+    //every time we run this useProductReducer() function, we receive two items in return: state is up to date and dispatch which we use to update the state
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        cart:[],
+        cartOpen: false,
+        categories: [],
+        currentCategory: "",
+    })
+    // confirm that everything works
+    console.log(state);
+    return <Provider value={[state, dispatch]}{...props} />;
+}
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
-
+//access the StoreContext in other components. 
 const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+    return useContext(StoreContext)
+}
 
-export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext }
